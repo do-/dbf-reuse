a5339dc34dc29228f28f2537d53b3694a7fb8fa4
Dmitry Ovsyanko <do_@pochta.ru>
2021-10-31 17:32:10 +0300

    `encoding` option, via iconv-lite

--- test/test.js
+++ test/test.js
@@ -2,14 +2,31 @@ const assert = require ('assert')
 const fs = require ('fs')
 const { Readable, Writable } = require ('stream')
 const {DBFHeader, DBFReader, DBFWriter} = require ('../')
-const iconv = require ('iconv-lite')
+const crypto = require ('crypto')
+
+const SRC_FILE_NAME = 'test/plat2.dbf'
+const DST_FILE_NAME = 'test/generated.dbf'
+const encoding = 'windows-1251'
 
 function getInputStream () {
-	return fs.createReadStream ('test/plat2.dbf')
+	return fs.createReadStream (SRC_FILE_NAME)
 }
 
 function getOutputStream () {
-	return fs.createWriteStream ('test/generated.dbf')
+	return fs.createWriteStream (DST_FILE_NAME)
+}
+
+async function checksum (fn) {
+
+	let h = crypto.createHash ('sha1')
+	let s = fs.createReadStream (fn)
+	
+	return new Promise ((ok, fail) => {
+		s.on ('error', fail)
+		s.on ('end', () => ok (h.digest ('hex')))
+		s.on ('data', b => h.update (b))
+	})
+
 }
 
 function test_001_header_check_date (h) {
@@ -40,7 +57,7 @@ async function test_001_header () {
 async function getRecords () {
 
 	let reader = new DBFReader ({
-		decoder: b => iconv.decode (b, 'win1251'),
+		encoding,
 		deletedFieldName: '_deleted',
 		nulls: true,
 	})
@@ -65,7 +82,7 @@ async function getRecords () {
 async function test_002_read () {
 
 	let records = await getRecords ()
-//console.log (records)
+
 	assert.strictEqual (records.length, 1)	
 	assert.strictEqual (records [0].name_strit, 'Дудикекера')
 	assert.strictEqual (records [0].d_fine, null)
@@ -76,19 +93,13 @@ async function test_003_write () {
 
 	let records = await getRecords ()
 
-	let writer = await DBFWriter.from (getInputStream (), {
-		encoder: s => iconv.encode (s, 'windows-1251'),
-	})
+	let writer = await DBFWriter.from (getInputStream (), {encoding})
 
 	Readable.from (records).pipe (writer).pipe (getOutputStream ())	
 	
-//	writer.pipe (getOutputStream ())	
+	let [checksum_src, checksum_dst] = await Promise.all ([SRC_FILE_NAME, DST_FILE_NAME].map (checksum))
 	
-//	for (let record of records) writer.write (record)
-	
-///	writer.end ()
-	
-//	await DBFWriter.copyHeader (getInputStream (), getOutputStream ())
+	assert.strictEqual (checksum_src, checksum_dst)
 
 }
 
